441cd206d963c6ca99caf72d4d7d08779b2e1012
josephcalderon6789-blip <josephcalderon6789@gmail.com>
2026-05-05 16:47:01 -0600

    Add files via upload

---
+++ codigo3.py
@@ -0,0 +1,15 @@
+#JOSEPH CALDERON
+#PROGRAMACIÓN 2 "A"
+#22/04/2026
+
+persona1 = float(input("persona 1: "))
+persona2 = float(input("persona 2: "))
+persona3 = float(input("perosona 3: "))
+
+total = persona1 + persona2 + persona3
+
+print("porcentaje 1", (persona1/total)*100)
+print("porcentaje 2", (persona2/total)*100)
+print("porcentaje 3", (persona3/total)*100)
+
+
