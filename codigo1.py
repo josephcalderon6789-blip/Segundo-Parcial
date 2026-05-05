4a76db70f7c7b420c6907866aa48cc348ffa58ab
josephcalderon6789-blip <josephcalderon6789@gmail.com>
2026-05-05 16:44:51 -0600

    Add files via upload

---
+++ codigo1.py
@@ -0,0 +1,13 @@
+#JOSEPH CALDERON
+#PROGRAMACIÓN 2 "A"
+#22/04/2026
+
+presupuesto = float(input("presupuesto anual"))
+
+ginecología = presupuesto * 0.40
+traumatología = presupuesto * 0.30
+pediatría = presupuesto * 0.30
+
+print("ginecologia", ginecologia)
+print("traumatología", traumatología)
+print("pediatría", pediatría)
