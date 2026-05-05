ec456aa797bb295ff67307116759e4cc357e81ea
josephcalderon6789-blip <josephcalderon6789@gmail.com>
2026-05-05 16:47:21 -0600

    Add files via upload

---
+++ codigo4.py
@@ -0,0 +1,13 @@
+#JOSEPH CALDERON
+#PROGRAMACIÓN 2 "A"
+#22/04/2026
+
+total = 0
+
+for i in range(5):
+    tiempo = float(input("tiempo del dia"))
+    total += tiempo
+
+promedio = total / 5
+
+print("tiempo promedio", promedio)
